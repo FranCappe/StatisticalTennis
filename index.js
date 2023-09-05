af495e99de24a0bab7cb1eb365cd1cd0c0a8775c
FranCappe <francappe13@gmail.com>
2023-09-05 15:18:56 -0300

    NUEVOS ARCHIVOS

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require ('express');
 const app = express();
 const path = require ('path');
 
+
 //MIDDLEWARE
 app.use(express.static(path.join(__dirname, './public')));
 app.use(express.json());
