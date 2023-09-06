afa0574b08c4b7a19c17d9970ff6ccb8d1f69acd
Franco <francappe13@gmail.com>
2023-09-06 20:04:06 -0300

    fetch y cosas varias

--- public/js/hits.js
+++ public/js/hits.js
@@ -5,18 +5,27 @@ window.onload = function(){
     const FORM = document.getElementById('hits-form')
     console.log(FORM)
 
-    FORM.addEventListener (submit, function(event){
+    FORM.addEventListener ("submit", function(event){
         event.preventDefault();
+        console.log("test")
+        let dateInput = FORM['date'].value;    
+        let forehandInput = FORM ['hit-forehand'].value;
+        let backhandInput = FORM ['hit-backhand'].value;
+        let forehandVolleyInput = FORM ['hit-forehand-volley'].value;
+        let backhandackhandVolleyInput = FORM ['hit-backhand-volley'].value;
+        let smashInput = FORM ['hit-smash'].value;
+        let sliceInput = FORM ['hit-slice'].value;
+        let dropInput = FORM ['hit-drop'].value;
+        console.log({dateInput, forehandInput, backhandInput,forehandVolleyInput, backhandackhandVolleyInput, smashInput, sliceInput, dropInput})
         
-    let idInput = FORM ['id'].value;
-    let dateInput = FORM ['date'].value;    
-    let forehandInput = FORM ['forehand'].value;
-    let backhandInput = FORM ['backhand'].value;
-    let forehandVolleyInput = FORM ['forehandVolley'].value;
-    let backhandackhandVolleyInput = FORM ['backhandVolley'].value;
-    let smashInput = FORM ['smash'].value;
-    let sliceInput = FORM ['slice'].value;
-    let dropInput = FORM ['drop'].value;
-   
+        
+        //El fetch para la api de hits 
 
+        fetch("/api/hits", {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
 })}
